b17c629297b46454b6d7a9ae862eb9a966d0e40c
wilderccr <48811404+wilderccr@users.noreply.github.com>
2019-07-20 19:06:52 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,50 @@
+package uiMain;
+
+import gestionAplicacion.usuarios.Usuario;
+import uiMain.menuConsola.*;
+import BaseDatos.Serializador;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Main {
+
+    public static final HashMap<String, OpcionDeMenu> listaOpciones = new HashMap<String, OpcionDeMenu>() {{
+        put("AnadirOpcion", new AnadirOpcion());
+        put("CambiarContrasena", new CambiarContrasena());
+        put("CerrarSesion", new CerrarSesion());
+        put("CrearCDT", new CrearCDT());
+        put("CrearCuentaCredito", new CrearCuentaCredito());
+        put("CrearCuentaDebito", new CrearCuentaDebito());
+        put("IniciarSesion", new IniciarSesion());
+        put("ManejarCuenta", new ManejarCuenta());
+        put("Registrarse", new Registrarse());
+        put("RemoverOpcion", new RemoverOpcion());
+        put("SalirDeLaAplicacion", new SalirDeLaAplicacion());
+        put("VerCuentas", new VerCuentas());
+        put("VerOpcionesDeUnUsuario", new VerOpcionesDeUnUsuario());
+        put("VerTodasLasOpciones", new VerTodasLasOpciones());
+        put("Contactenos", new Contactenos());
+    }};
+    public static Usuario usuario;
+    public static HashMap<String, Usuario> usuarios;
+
+    public static void main(String[] args) throws IOException {
+        // Todos los usuarios y administradores registrados en la base de datos
+        usuarios = Serializador.cargar();
+        definirInvitado();
+        
+            usuario.getMenu().encabezado();
+
+        for (;;)
+            usuario.getMenu().lanzarMenu();
+    }
+
+    public static final void definirInvitado() { usuario = new Usuario(); }
+
+    // Temporal
+    public static final void mostrarUsuarios() {
+        for (Usuario user : usuarios.values()) System.out.println(user);
+        System.out.println();
+    }
+}
---
+++ MenuDeConsola.java
@@ -0,0 +1,58 @@
+package uiMain;
+
+import java.io.BufferedInputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class MenuDeConsola implements Serializable {
+    private ArrayList<OpcionDeMenu> opciones = new ArrayList<OpcionDeMenu>() {{
+        add(new uiMain.menuConsola.SalirDeLaAplicacion());
+    }};
+
+    public MenuDeConsola(String[] menu) {
+        for (String opcion : menu)
+            anadirOpcion(Main.listaOpciones.get(opcion));
+    }
+
+    public void anadirOpcion(OpcionDeMenu opcion) { opciones.add(opcion); }
+    
+    void encabezado() {
+    	String titulo = "<<<<<<<<<<<<<<<<<<<<<<<<<<<< Bienvenido a Green Bank >>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n" + 
+        	    "     $                                                                    $" + "\n"
+        	    + "  $  $  $                                                              $  $  $" + "\n"
+        	    + "   $ $ $            Un banco al alcance de sus posibilidades            $ $ $" + "\n"
+        	    + "     $                                                                    $" + "\n"
+        	    + "     $____________________________________________________________________$" + "\n\n\n\"";
+    	System.out.println(titulo);
+    }
+
+    /* Se muestra el conjunto de opciones asociadas al correspondiente menu de
+     * consola y se recibe entrada por parte del usuario (I/O) */
+    void lanzarMenu() {
+        String opcion = null, margen = "\n\n\n";
+        Scanner input = new Scanner(new BufferedInputStream(System.in));
+        OpcionDeMenu[] arregloOpciones = opciones.toArray(new OpcionDeMenu[opciones.size()]);
+
+        do {
+            System.out.format(margen + "%-25s: " + Main.usuario.getNombre() + "%n%n%-25s%s%n", "Usuario", "OPCIÓN",
+                    "NÚMERO");
+
+            // Temporal
+            Main.mostrarUsuarios();
+
+            for (byte i = -1; ++i < arregloOpciones.length; )
+                System.out.format("%-25s%d%n", arregloOpciones[i], i + 1);
+
+            if (opcion != null)
+                System.out.println("\n\"" + opcion + "\" no corresponde a ningún número de opción válido.");
+
+            System.out.print("\nIngrese el número de la opción a ejecutar: ");
+        } while (!Pattern.compile("\\d+").matcher(opcion = input.next()).matches() || Integer.parseInt(opcion) < 1
+                || Integer.parseInt(opcion) > arregloOpciones.length);
+
+        arregloOpciones[Integer.parseInt(opcion) - 1].ejecutar();
+    }
+
+}
