5f43cfa5871263554348bfd8000f146c661791b5
johan cano <joscanoga@unal.edu.co>
2019-08-27 16:03:06 -0500

    .

---
+++ GreenBank.java
@@ -0,0 +1,10 @@
+import view.*;
+import controller.*;
+import gestionAplicacion.usuarios.*;
+public class GreenBank {
+ public static void main(String[] args) {
+	 Usuario usuario=new UsuarioInvitado();
+	 System.out.println(usuario);
+	 VentanaAplicacion vista= new VentanaAplicacion();
+ }
+}
